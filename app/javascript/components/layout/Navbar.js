e2788d05f94b23cc1e59119f6e6e8d6b32c7d368
Brian Bower <brian@brianbower.com>
2020-03-26 15:01:52 -0600

    Add navbar component

---
+++ app/javascript/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class Navbar extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <nav className="navbar">
+          <h1>LOGO</h1>
+          <h1>SumoCity</h1>
+        </nav>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Navbar
