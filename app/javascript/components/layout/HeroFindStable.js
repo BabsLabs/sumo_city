d8e14ad8343acb57eee490bdef54a6053d9af06b
Brian Bower <brian@brianbower.com>
2020-04-04 11:25:11 -0600

    Add homepage heroFindStable component

---
+++ app/javascript/components/layout/HeroFindStable.js
@@ -0,0 +1,21 @@
+import React from "react"
+import DemoMapboxImage from "../images/DemoMapboxImage"
+
+class HeroFindStable extends React.Component {
+  render() {
+    return (
+      <div className="hero heroFindStable">
+          <section className="heroText">
+            <h1>Stable Explorer</h1>
+            <p>The place where Sumos train is called a Stable.</p>
+            <p>Use the interactive Stable Explorer to learn more about the stables of all your favorite sumo wrestlers.</p>
+          </section>
+          <section className="heroImage">
+            <img src={DemoMapboxImage} alt="MapboxImage"/>
+          </section>
+      </div>
+    )
+  }
+}
+
+export default HeroFindStable
