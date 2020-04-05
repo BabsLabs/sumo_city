b3fa0383d95efec773107bbeb76e36a75be34061
Brian Bower <brian@brianbower.com>
2020-04-05 17:38:04 -0600

    Rearrange image and text so homepage components alternate

--- app/javascript/components/layout/homepage/HeroFindStable.js
+++ app/javascript/components/layout/homepage/HeroFindStable.js
@@ -5,13 +5,13 @@ class HeroFindStable extends React.Component {
   render() {
     return (
       <div className="hero heroFindStable">
+          <section className="heroImage">
+            <img src={DemoMapboxImage} alt="MapboxImage"/>
+          </section>
           <section className="heroText">
             <h1>Stable Explorer</h1>
             <p>The place where Sumos train is called a Stable.</p>
             <p>Use the interactive Stable Explorer to learn more about the stables of all your favorite sumo wrestlers.</p>
-          </section>
-          <section className="heroImage">
-            <img src={DemoMapboxImage} alt="MapboxImage"/>
           </section>
       </div>
     )
