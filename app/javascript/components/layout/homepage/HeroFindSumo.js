b1e1e08f7e74ad59edf0510dcbc02366c087df96
Brian Bower <brian@brianbower.com>
2020-04-05 17:37:15 -0600

    Add component to find sumo on homepage

---
+++ app/javascript/components/layout/homepage/HeroFindSumo.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+class HeroFindSumo extends React.Component {
+  render() {
+    return (
+      <div className="hero heroFindSumo">
+        <section className="heroText">
+          <h1>Find Your Favorite Sumo</h1>
+          <p>Learn more about all your favorite sumo wrestlers.</p>
+        </section>
+        <section className="heroImage">
+          <h1>SUMO CARD IMAGE</h1>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default HeroFindSumo
