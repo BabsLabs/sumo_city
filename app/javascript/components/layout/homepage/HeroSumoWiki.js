27668ddcd97e7b72b7aa46ec6c05f6fd3701fca3
Brian Bower <brian@brianbower.com>
2020-04-05 18:00:16 -0600

    Add wiki component for homepage

---
+++ app/javascript/components/layout/homepage/HeroSumoWiki.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+class HeroSumoWiki extends React.Component {
+  render() {
+    return (
+      <div className="hero heroSumoWiki">
+        <section className="heroText">
+          <h1>Learn More About Everything Sumo</h1>
+          <p>Use our Sumo Wiki to learn more about the rules, history, and vocab of Sumo.</p>
+        </section>
+        <section className="heroText">
+          <h1>Word of the Day</h1>
+          <p>Oshidashi: A frontal push out where the offensive wrestler pushes out their opponent maintaining hand contact at all times.</p>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default HeroSumoWiki
