ac446af5791dc40287ad5a9b053d6eea4aeff710
Brian Bower <brian@brianbower.com>
2020-04-13 14:39:08 -0600

    Update describe block to clarify. Add "about" page test test.

--- spec/react_spec.rb
+++ spec/react_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
-describe "GET '/' - homepage title", :type => :feature, js: true do
-  it 'can check the page title' do
+describe "React testing", :type => :feature, js: true do
+  it 'can check the Homepage for content' do
     visit('/')
     expect(page.title).to have_content("SumoCity")
     puts 'the homepage title is "SumoCity"'
     expect(page.body).to have_content("SumoCity")
-    puts 'cool, capybara can read the title somewhere on the home page!'
+    puts 'cool, capybara can read "SumoCity" somewhere on the homepage!'
     # expect(page.body).to have_content("Stable Explorer") # THIS DOES NOT PASS!!!
+  end
+
+  it 'can check the About page for content' do
+    visit('/about')
+    # expect(page).to have_css("about") # THIS ALSO DOES NOT PASS!!!
+    puts 'cool, capybara can read "About SumoCity: somewhere on the about page!'
   end
 end
