d277877f50f26b91087e1a49ec0695721139b13f
Brian Bower <brian@brianbower.com>
2020-04-13 17:16:52 -0600

    Refactor tests into dedicated directories

--- spec/features/about/about_spec.rb
+++ spec/features/about/about_spec.rb
@@ -1,16 +1,5 @@
 require 'rails_helper'
 
-describe "React homepage testing", :type => :feature, js: true do
-  it 'can check the Homepage for content' do
-    visit('/')
-    expect(page.title).to have_content("SumoCity")
-    expect(page).to have_content("SumoCity")
-    expect(page).to have_content("Stable Explorer")
-    expect(page).to have_content("The place where Sumos train is called a Stable.")
-    expect(page).not_to have_content("Sumo city is your homepage for all things Sumo")
-  end
-end
-
 describe "React About testing", :type => :feature, js: true do
   it 'can check the About page for content' do
     visit('/about')
