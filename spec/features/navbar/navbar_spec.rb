97a88eabdb59012360187917ef8177897d024a1d
Brian Bower <brian@brianbower.com>
2020-04-14 15:16:10 -0600

    Add navbar test

---
+++ spec/features/navbar/navbar_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "React About testing", :type => :feature, js: true do
+  it 'can check the About page for content' do
+    visit('/')
+    within ".navbar" do
+      expect(page).to have_content("SumoCity")
+      expect(page).to have_link("About")
+      expect(page).to have_link("Home")
+      expect(page).to have_css("#navbar-sumoLogo")
+    end
+  end
+end
