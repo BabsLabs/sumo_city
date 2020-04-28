3e39d74814fe9e4d5b0cdd07b7aa4ecde72319df
Brian Bower <brian@brianbower.com>
2020-04-28 16:00:56 -0600

    Add tests to check for Featured Term text

--- spec/features/index/index_spec.rb
+++ spec/features/index/index_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe "React homepage testing", :type => :feature, js: true do
   it 'can check the Homepage for content' do
+    @term = Term.create!(english_name: "Kimetaoshi", japanese_name: "極め倒し", definition: "Immobilizing the opponent's arms and shoulders with one's arms and forcing him down (arm barring force down).")
+
     visit('/')
     expect(page.title).to have_content("SumoCity")
 
@@ -16,7 +18,10 @@ describe "React homepage testing", :type => :feature, js: true do
     within "#heroSumoWiki" do
       expect(page).to have_content("Learn More About Everything Sumo")  
       expect(page).to have_content("Use our Sumo Wiki to learn more about the rules, history, and vocab of Sumo.")  
-      expect(page).to have_content("Word of the Day")
+      expect(page).to have_content("Featured Term")
+      expect(page).to have_content(@term.english_name)
+      expect(page).to have_content(@term.japanese_name)
+      expect(page).to have_content(@term.definition)
     end
 
     # Make sure About component content is not being rendered
