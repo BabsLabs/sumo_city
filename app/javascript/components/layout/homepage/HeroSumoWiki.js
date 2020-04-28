64bcd667f6342afcec1b8fd7fb758877da48a2ad
Brian Bower <brian@brianbower.com>
2020-04-28 15:24:21 -0600

    Add featured term prop and require proptypes to display a random featured term

--- app/javascript/components/layout/homepage/HeroSumoWiki.js
+++ app/javascript/components/layout/homepage/HeroSumoWiki.js
@@ -1,20 +1,29 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 class HeroSumoWiki extends React.Component {
   render() {
     return (
-      <div className="hero" id="heroSumoWiki">
-        <section className="heroText">
-          <h1>Learn More About Everything Sumo</h1>
-          <p>Use our Sumo Wiki to learn more about the rules, history, and vocab of Sumo.</p>
-        </section>
-        <section className="heroText">
-          <h1>Word of the Day</h1>
-          <p>Oshidashi: A frontal push out where the offensive wrestler pushes out their opponent maintaining hand contact at all times.</p>
-        </section>
-      </div>
+      <React.Fragment>
+        <div className="hero" id="heroSumoWiki">
+          <section className="heroText">
+            <h1>Learn More About Everything Sumo</h1>
+            <p>Use our Sumo Wiki to learn more about the rules, history, and vocab of Sumo.</p>
+          </section>
+          <section className="heroText">
+            <h1>Featured Term</h1>
+            <h3>{this.props.term.english_name}</h3>
+            <h4>{this.props.term.japanese_name}</h4>
+            <p>{this.props.term.definition}</p>
+          </section>
+        </div>
+      </React.Fragment>
     )
   }
 }
+
+HeroSumoWiki.propTypes = {
+  term: PropTypes.string
+};
 
 export default HeroSumoWiki
