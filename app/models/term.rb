864eac5fbb09c235b7e6b97385b70065adcfcd2b
Brian Bower <brian@brianbower.com>
2020-04-29 17:41:11 -0600

    Add validates_presence and move format name to private method

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,10 +1,15 @@
 class Term < ApplicationRecord
-
   before_validation :format_name
 
-  def format_name
-    self.english_name = self.english_name.capitalize
-    self.japanese_name = self.japanese_name.gsub(/[()]/, "")
-  end
+  validates_presence_of :english_name, :japanese_name, :definition
+  
+  private 
 
+    def format_name
+      if english_name.present? || japanese_name.present? || definition.present?
+        self.english_name = self.english_name.capitalize
+        self.japanese_name = self.japanese_name.gsub(/[()]/, "")
+        self.definition = self.definition.capitalize
+      end
+    end
 end
