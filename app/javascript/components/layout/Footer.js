d508128482ec8d43d8b7eca725339a4ac89f7b1c
Brian Bower <brian@brianbower.com>
2020-04-30 11:55:40 -0600

    Add footer React component to display copyright info

---
+++ app/javascript/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Footer extends React.Component {
+  render () {
+    return (
+        <footer className="footer">
+          <p>&copy;	BabsLabs Software 2020</p>
+        </footer>
+    );
+  }
+}
+
+export default Footer
