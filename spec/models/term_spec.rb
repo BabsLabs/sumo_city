dffeec0e449fa5502c0cd1c33e093a41b5f42ca9
Brian Bower <brian@brianbower.com>
2020-05-01 16:08:39 -0600

    Update validations to check on create or update due how the raketask and shoulda matchers work together

--- spec/models/term_spec.rb
+++ spec/models/term_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 describe Term do
   describe 'validations' do
-    it { should validate_presence_of(:english_name) }
-    it { should validate_presence_of(:japanese_name) }
-    it { should validate_presence_of(:definition) }
+    it { should validate_presence_of(:english_name).on(:create) }
+    it { should validate_presence_of(:english_name).on(:update) }
+    it { should validate_presence_of(:japanese_name).on(:create) }
+    it { should validate_presence_of(:japanese_name).on(:update) }
+    it { should validate_presence_of(:definition).on(:update) }
+    it { should_not validate_presence_of(:definition).on(:create) }
   end
 
   describe "before_action (capitalize and gsub parenthesis)" do
     it 'should update names for a new record' do
-      @term = Term.new(english_name: 'kimetaoshi', japanese_name: '(極め倒し)', definition: "immobilizing the opponent's...")
-      @term.valid?
+      @term = Term.create(english_name: 'kimetaoshi', japanese_name: '(極め倒し)', definition: "immobilizing the opponent's...")
       expect(@term.english_name).to  eq('Kimetaoshi') # Name has been capitalized
       expect(@term.japanese_name).to  eq('極め倒し') # Parenthesis have been removed
       expect(@term.definition).to  eq("Immobilizing the opponent's...") # Definition has been capitalized
