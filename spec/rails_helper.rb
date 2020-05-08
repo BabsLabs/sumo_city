941854fd63805066acc78632878df1f9aaf971fb
Brian Bower <brian@brianbower.com>
2020-05-08 10:24:17 -0600

    Update capybara to use firefox instead of chrome

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,24 +31,38 @@ require 'capybara/dsl'
 # https://www.reddit.com/r/ruby/comments/8d6vdb/capybara_rails_chromeheadless_on_travis/
 require "selenium/webdriver"
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
+
+# Capybara.register_driver :headless_chrome do |app|
+#   options = Selenium::WebDriver::Chrome::Options.new
+
+#   options.add_argument('--headless')
+#   options.add_argument('--no-sandbox')
+#   options.add_argument('--disable-popup-blocking')
+#   options.add_argument('--window-size=1366,768')
+
+#   driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+
+#   driver
+# end
+
+# Capybara.javascript_driver = :headless_chrome
+
+Capybara.register_driver :selenium_headless do |app|
+  browser_options = ::Selenium::WebDriver::Firefox::Options.new()
+  browser_options.args << '--headless'
+  browser_options.args << '--disable-popup-blocking'
+  browser_options.args << '--window-size=1366,768'
+  browser_options.args << '--no-sandbox'
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :firefox,
+    options: browser_options
+  )
 end
-
-Capybara.register_driver :headless_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-popup-blocking')
-  options.add_argument('--window-size=1366,768')
-
-  driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-
-  driver
-end
-
-Capybara.javascript_driver = :headless_chrome
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
