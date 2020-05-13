824283ca2265f6cf234e75ca13ad213ed23fa428
Brian Bower <brian@brianbower.com>
2020-05-13 15:19:43 -0600

    Add stable active record query and feature geojson render

--- app/controllers/explorer_controller.rb
+++ app/controllers/explorer_controller.rb
@@ -1,4 +1,29 @@
 class ExplorerController < ApplicationController
   def index
+    @stables = Stable.all
+    @geojson = Array.new
+    
+    @stables.each do |stable|
+      @geojson << {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [stable.lon, stable.lat]
+        },
+        properties: {
+          tile: stable.title,
+          # :'marker-color' => stable.hexcolor,
+          # :'marker-symbol' => 'circle',
+          # :'marker-size' => 'medium'
+        }
+      }
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @geojson }  # respond with the created JSON object
+    end
+
+
   end
 end
