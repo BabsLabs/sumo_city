d89ec98d58c423dad06c51de2d15c2616471c160
Brian Bower <brian@brianbower.com>
2020-05-14 22:01:29 -0600

    Add index actions for homepage explorer component

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,26 @@
 class IndexController < ApplicationController
   def index
     @term = Term.all.sample(1).first
+    @stables = Stable.all
+    @geojson_features = Array.new
+    
+    @stables.each do |stable|
+      @geojson_features << {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [stable.lon, stable.lat]
+        },
+        properties: {
+          title: stable.title,
+          color: stable.hexcolor
+        }
+      }
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @geojson_features }  # respond with the created JSON object
+    end
   end
 end
