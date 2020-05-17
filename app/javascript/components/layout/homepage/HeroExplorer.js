f36b99c941474c97473d6c4a6e5b6c8aed2def68
Brian Bower <brian@brianbower.com>
2020-05-17 12:57:42 -0600

    change sections to divs

--- app/javascript/components/layout/homepage/HeroExplorer.js
+++ app/javascript/components/layout/homepage/HeroExplorer.js
@@ -6,13 +6,13 @@ class HeroExplorer extends React.Component {
   render() {
     return (
       <div className="hero" id="heroExplorer">
-          <section className="heroText">
+          <div className="heroText">
             <h1>SumoCity Explorer</h1>
             <p>Use the interactive <a href="/explorer">SumoCity Explorer</a> to learn more about the stables of all your favorite sumo wrestlers.</p>
-          </section>
-          <section id="heroMap">
+          </div>
+          <div id="heroMap">
             <Mapbox geojson_features={this.props.geojson_features} stables={this.props.stables} height={this.props.height} width={this.props.width} className="homepageMap"/>
-          </section>
+          </div>
       </div>
     )
   }
