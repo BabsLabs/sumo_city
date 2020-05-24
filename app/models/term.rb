875aa4d50243f6e26157f855a3e3cf3c96bd3a3c
Brian Bower <brian@brianbower.com>
2020-05-24 10:52:00 -0600

    Remove before validation action because the data is now pre-cleaned

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,14 +1,3 @@
 class Term < ApplicationRecord
-  before_validation :format_name
-
-  validates_presence_of(:english_name, :japanese_name, on: [:create, :update])
-  validates_presence_of(:english_name, :japanese_name, :definition, on: :update)
-  
-  private 
-
-    def format_name
-        self.english_name = self.english_name.lstrip.capitalize if english_name.present?
-        self.japanese_name = self.japanese_name.gsub(/[()]/, "") if japanese_name.present?
-        self.definition = self.definition.lstrip.capitalize if definition.present?
-    end
+  validates_presence_of(:english_name, :japanese_name, :definition, on: [:create, :update])
 end
