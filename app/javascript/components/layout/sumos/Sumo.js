869c70742894613ef2e506a641ec30eae405c5c0
Brian Bower <brian@brianbower.com>
2020-05-28 09:09:49 -0600

    Add card with placeholder image

--- app/javascript/components/layout/sumos/Sumo.js
+++ app/javascript/components/layout/sumos/Sumo.js
@@ -1,9 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
+import placeHolderImage from "../../images/SumoLogo.png"
 class Sumo extends React.Component {
   render () {
     return (
-        <div className="sumo" id={"sumo-" + this.props.sumo.id}>
+      <div className="hero">
+        <div id={"card-" + this.props.sumo.id} className="card">
+          <img src={placeHolderImage} alt="" style={{
+            "border": "20px solid" + this.props.stable.hexcolor,
+            "padding": "5px"
+          }}/>
+        </div>
+        <div className="heroText" id={"sumo-" + this.props.sumo.id}>
           <p>Ring Name(s): {this.props.sumo.ring_name}</p>
           <p>Heya (Stable): <a href={"/stables/" + this.props.stable.id}>{this.props.stable.title}</a></p>
           <p>Rank: {this.props.sumo.rank}</p> 
@@ -12,6 +20,7 @@ class Sumo extends React.Component {
           <p>Height: {this.props.sumo.height}</p>
           <p>Weight: {this.props.sumo.weight}</p>
         </div>
+      </div>
     );
   }
 }
