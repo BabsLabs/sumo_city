6e2b0888b4650ceea9c1d0843ccd3b14f7b373a7
Brian Bower <brian@brianbower.com>
2020-06-03 15:11:55 -0600

    Add flex-center class to center the about image

--- app/javascript/components/layout/About.js
+++ app/javascript/components/layout/About.js
@@ -5,7 +5,7 @@ class About extends React.Component {
   render() {
     return (
       <div>
-          <div id="about-image">
+          <div id="about-image" className="flex-center">
             <img src={customSumoLogo} />
           </div>
           <div id="about">
