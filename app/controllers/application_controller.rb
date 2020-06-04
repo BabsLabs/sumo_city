f2ade71646becd210bd47e0f58e0984295640f96
Brian Bower <brian@brianbower.com>
2020-06-04 13:57:35 -0600

    Add make_stables_geojson(method) which is used by a number of controllers to dry up code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+
+  def make_stables_geojson(stable_collection)    
+    geojson_features = Array.new
+
+    stable_collection.each do |stable|
+    geojson_features << {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [stable.lon, stable.lat]
+        },
+        properties: {
+          title: stable.title,
+          color: stable.hexcolor
+        }
+      }
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: geojson_features }  # respond with the created JSON object
+    end
+  end
+
 end
