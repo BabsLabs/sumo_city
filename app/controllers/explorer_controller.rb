bd74b7ae96208f78de91fc185ccc4a82a8491267
Brian Bower <brian@brianbower.com>
2020-06-04 13:57:12 -0600

    Update show action to use make_stables_geojson method inherited from application_controller to dry up code

--- app/controllers/explorer_controller.rb
+++ app/controllers/explorer_controller.rb
@@ -1,25 +1,6 @@
 class ExplorerController < ApplicationController
   def index
     @stables = Stable.all
-    @geojson_features = Array.new
-    
-      @stables.each do |stable|
-      @geojson_features << {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [stable.lon, stable.lat]
-        },
-        properties: {
-          title: stable.title,
-          color: stable.hexcolor
-        }
-      }
-    end
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @geojson_features }  # respond with the created JSON object
-    end
+    @geojson_features = make_stables_geojson(@stables)
   end
 end
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,25 +5,6 @@ class IndexController < ApplicationController
     @term = Term.all.sample(1).first
     
     @stables = Stable.all
-    @geojson_features = Array.new
-    
-      @stables.each do |stable|
-      @geojson_features << {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [stable.lon, stable.lat]
-        },
-        properties: {
-          title: stable.title,
-          color: stable.hexcolor
-        }
-      }
-    end
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @geojson_features }  # respond with the created JSON object
-    end
+    @geojson_features = make_stables_geojson(@stables)
   end
 end
