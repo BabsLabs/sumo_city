179199b7de6e36ea6a9521e6bee16c5fa6adc1af
Brian Bower <brian@brianbower.com>
2020-06-05 12:41:23 -0600

    Add exception handling to the routes

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module SumoCity
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Custom error pages
+    config.exceptions_app = self.routes
   end
 end
